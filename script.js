33a0f703d2e62021191aeaf9b422feb7e4cb0e27
timi-boy <timi.biz@bk.ru>
2021-07-24 16:34:28 +0400

    сделал Добавление книги

--- script.js
+++ script.js
@@ -1,30 +1,77 @@
-const url = 'https://apiinterns.osora.ru/ '
-const form = document.querySelector('.upload')
+const listBooks = JSON.parse(localStorage.getItem('listBooks')) ?? [];
+const formUpload = document.querySelector('.upload');
 
-form.addEventListener('submit', (e) => {
+// Добавить книгу
+
+const radioAdd = document.querySelector('#add-book');
+const radioWrite = document.querySelector('#write-book');
+
+const formWrite = document.querySelector('.write');
+const submit = document.querySelector('.write__submit');
+const title = document.querySelector('.write__title-book');
+const text = document.querySelector('.write__text-book');
+
+radioAdd.addEventListener('change', () => {
+  if (radioAdd.checked) {
+    console.log('true');
+    formUpload.style.display = 'block';
+    formWrite.style.display = 'none';
+  };
+});
+
+radioWrite.addEventListener('change', () => {
+  if (radioWrite.checked) {
+    console.log('true');
+    formWrite.style.display = 'block';
+    formUpload.style.display = 'none';
+  };
+})
+
+// Загрузить файл
+
+const url = 'https://apiinterns.osora.ru/ '
+
+formUpload.addEventListener('submit', async (e) => {
   e.preventDefault()
 
-  const files = document.querySelector('.file').files
-  const formData = new FormData()
+  const file = document.querySelector('.file').files[0];
+  const formData = new FormData();
 
   formData.append('login', 'String');
+  formData.append('file', file);
 
-  //for (let i = 0; i < files.length; i++) {
-  //  let file = files[i]
-
-  //}
-  formData.append('file', files);
-
-  console.log(formData);
-
-  for(let [name, value] of formData) {
-    alert(`${name} = ${value}`); // key1=value1, потом key2=value2
-  }
-
-  fetch(url, {
+  let response = await fetch(url, {
     method: 'POST',
     body: formData,
-  }).then((response) => {
-    console.log(response)
-  })
-})
+  });
+
+  let result = await response.json();
+  console.log(result);
+
+  let book = {
+    title: result.title,
+    text: result.text
+  };
+
+  listBooks.push(book);
+  console.log(listBooks);
+  //localStorage.setItem('listBooks', JSON.stringify(listBooks));
+});
+
+// Написать книгу
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault(); 
+  let obj = {
+    title: title.value,
+    text: text.value
+  };
+  console.log(obj);
+  listBooks.push(obj);
+  console.log(listBooks);
+}) ;
+
+
+//window.addEventListener('beforeunload', () => {
+//  localStorage.setItem('listBooks', JSON.stringify(listBooks));
+//});
