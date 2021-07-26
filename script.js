ab98f08d9ec81569ef4b09571fba3d431e1f2daf
timi-boy <timi.biz@bk.ru>
2021-07-26 16:47:51 +0400

    Закончил список книг

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const submit = document.querySelector('.write__submit');
 const title = document.querySelector('.write__title-book');
 const text = document.querySelector('.write__text-book');
 
+const mainContainer = document.querySelector('.container');
+const bookBox = document.createElement('div');
+mainContainer.after(bookBox);
+
 radioAdd.addEventListener('change', () => {
   if (radioAdd.checked) {
     console.log('true');
@@ -50,13 +54,17 @@ formUpload.addEventListener('submit', async (e) => {
 
   let book = {
     title: result.title,
-    text: result.text
+    text: result.text,
+    date: Number(new Date()),
+    read: false
   };
 
   listBooks.push(book);
   console.log(listBooks);
+
   //localStorage.setItem('listBooks', JSON.stringify(listBooks));
   listBooksAdd(book);
+  //listBooksAdd(book);
 });
 
 // Написать книгу
@@ -65,11 +73,14 @@ submit.addEventListener('click', (e) => {
   e.preventDefault(); 
   let book = {
     title: title.value,
-    text: text.value
+    text: text.value,
+    date: Number(new Date()),
+    read: false
   };
   console.log(book);
   listBooks.push(book);
   console.log(listBooks);
+
   listBooksAdd(book);
 });
 
@@ -86,15 +97,82 @@ function listBooksAdd(book) {
   const clear = document.createElement('button');
   clear.className = 'list__clear';
   clear.innerHTML = 'X';
+  clear.style.cursor = 'pointer';
   item.append(clear);
   clear.addEventListener('click', () => {
-    
-    listBooks.splice(item.remove(), 1)
+    item.remove();
+    for (let i = 0; i < listBooks.length; i++) {
+      if (listBooks[i].date === book.date) {
+        listBooks.splice(i, 1);
+        break;
+      }
+    };
     console.log(listBooks);
+  });
+
+  const finished = document.createElement('button');
+  finished.className = 'list__finished';
+  finished.innerHTML = 'прочитано';
+  finished.style.cursor = 'pointer';
+  clear.after(finished);
+  finished.addEventListener('click', () => {
+    for (let i = 0; i < listBooks.length; i++) {
+      if (listBooks[i].date === book.date) {
+        if (listBooks[i].read === false) {
+          listBooks[i].read = true
+          item.style.backgroundColor = 'rgb(199, 167, 131, 0.7)';
+        } else {
+          listBooks[i].read = false;
+          item.style.backgroundColor = '#fff';
+        };
+        break;
+      }
+    };
+    
+    function fn(a,b) {
+      if (a.read === false && b.read === false) { return a.date - b.date };
+      if (a.read === true && b.read === true) { return a.date - b.date };
+      if (a.read === false && b.read === true) { return b.read - a.read };
+      if (a.read === true && b.read === false) { return b.read - a.read };
+    };
+    listBooks.sort(fn);
+    console.log(listBooks);
+  });
+  if (book.read === true) {
+    item.style.backgroundColor = 'rgb(199, 167, 131, 0.7)';
+  };
+  
+  const read = document.createElement('button');
+  read.className = 'list__read';
+  read.innerHTML = 'читать';
+  read.style.cursor = 'pointer';
+  finished.after(read);
+  read.addEventListener('click', () => {
+    bookBox.className = 'list__book-box';
+    bookBox.innerHTML = `<p>${book.text}</p>`;
+  });
+
+  const edit = document.createElement('button');
+  edit.className = 'list__edit';
+  edit.innerHTML = 'редактировать';
+  edit.style.cursor = 'pointer';
+  read.after(edit);
+  edit.addEventListener('click', () => {
+    bookBox.className = 'list__book-box';
+    bookBox.innerHTML = `<p class="edited-text" contenteditable="true">${book.text}</p>`;
+    const editedText = document.querySelector('.edited-text');
+
+    const ok = document.createElement('button');
+    ok.innerHTML = 'OK';
+    bookBox.append(ok);
+    ok.addEventListener('click', () => {
+      book.text = editedText.innerHTML
+      console.log(book.text);
+    });
   });
 };
 
-listBooks.forEach(book => listBooksAdd(book));
+listBooks.forEach((book, id) => listBooksAdd(book, id));
 
 window.addEventListener('beforeunload', () => {
   localStorage.setItem('listBooks', JSON.stringify(listBooks));
